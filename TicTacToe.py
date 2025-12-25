dde9d83a612b3ea10e73ea975986914f8a9b6070
GodunicornIzek <godunicornizek@gmail.com>
2025-12-25 06:50:16 +0000

    Update TicTacToe.py: wrap demo code in __main__

---
+++ TicTacToe.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Creating a TicTacToe Class
+
+# In[ ]:
+
+
+import numpy as np
+np.__version__
+
+
+# In[ ]:
+
+
+class TicTacToe:
+    def __init__(self):
+        self.row_count = 3
+        self.column_count = 3
+        self.action_size = self.row_count * self.column_count
+        # self.action_correspondence = np.arange(0, action_size).reshape(self.row_count, self.column_count)
+
+    def get_init(self) -> np.ndarray:
+        return np.zeros((self.row_count, self.column_count))
+
+    def get_next_state(self, state: np.ndarray, action: int, player: int) -> np.ndarray:
+        row = action // self.column_count
+        column = action % self.column_count
+        state[row, column] = player
+        return state
+        # return np.where(state == 0, player, state)
+
+    # def determine_legal(self, state: np.ndarray, action: int) -> bool:
+    #     row = action // self.column_count
+    #     column = action % self.column_count
+    #     if state[row, column] != 0:
+    #         return False
+    #     return True
+
+    def get_valid_moves(self, state: np.ndarray):
+        return (state.reshape(-1) == 0).astype(np.uint8)
+
+    def determine_won(self, state: np.ndarray, action: int) -> bool:
+        row = action // self.column_count
+        column = action % self.column_count
+        player = state[row, column]
+
+        return (
+             np.sum(state[row, :] == player * self.column_count)
+             or np.sum(state[:, column]) == player * self.row_count
+             or np.sum(np.diag(state)) == player * self.row_count
+             or np.sum(np.diag(np.fliplr(state))) == player * self.row_count
+        )
+
+    def get_value_and_terminated(self, state, action):
+        if self.determine_won(state, action):
+            return 1, True
+        elif np.sum(self.get_valid_moves(state)) == 0:
+            return 0, True
+        else:
+            return 0, False
+
+    def get_opponent(self, player):
+        return -player
+
+
+# # Testing TicTacToe
+
+# In[ ]:
+
+
+# Initializing the board and players
+tictactoe = TicTacToe()
+player = 1
+state = tictactoe.get_init()
+opponent = tictactoe.get_opponent
+
+
+# In[ ]:
+
+
+# Starting for 1 turn
+action = 4
+state = tictactoe.get_next_state(state, action, player)
+print(state)
+value, terminated = tictactoe.get_value_and_terminated(state, action)
+print(f"The game has {'terminated' if bool(terminated) else 'not terminated'}.")
+
+
+# In[ ]:
+
+
+import random
+
+tictactoe2 = TicTacToe()
+player = 1
+state = tictactoe.get_init()
+opponent = tictactoe.get_opponent
+
+while True:
+    print(state)
+    valid_moves = tictactoe2.get_valid_moves(state)
+    print("valid moves", [i for i in range(tictactoe2.action_size) if valid_moves[i] == 1])
+    action = int(input(f"{player}:"))
+
+    if valid_moves[action] == 0:
+        print("Action not valid")
+        continue
+
+    state = tictactoe2.get_next_state(state, action, player)
+
+    value, is_terminated = tictactoe2.get_value_and_terminated(state, action)
+
+    if is_terminated:
+        print(state)
+        if value == 1:
+            print(player, "won")
+        else:
+            print("draw")
+        break
+
+    player = tictactoe2.get_opponent(player)
+
+
+# # Github Pushing
+
+# In[ ]:
+
+
+get_ipython().system('git clone https://github.com/GodunicornIzek/SelfPlayAI.git')
+
+
+# In[ ]:
+
+
+get_ipython().system('git config --global user.email "godunicornizek@gmail.com"')
+get_ipython().system('git config --global user.name "GodUnicornIzek"')
+
+
+# In[ ]:
+
+
+# Save the current notebook into /content with a specific name
+from google.colab import drive, files, _import_hooks
+import os
+
+# If you want to save the current notebook with a known name:
+get_ipython().system('cp "/content/AlphaZeroImplementation.ipynb" "/content/AlphaZeroImplementation.ipynb"')
+
+
+# In[ ]:
+
+
+from google.colab import files
+
+# This will prompt you to upload a file if you want to bring it from your local machine
+uploaded = files.upload()
+
+
+# In[ ]:
+
+
+get_ipython().system('ls /content/')
+
+
+# In[ ]:
+
+
+get_ipython().system('cp /content/AlphaZeroImplementation.ipynb /content/SelfPlayAI/')
+
+
+# In[ ]:
+
+
+# Instead of hardcoding the token:
+import getpass
+token = getpass.getpass("Enter GitHub token: ")
+
+get_ipython().system('git push https://GodUnicornIzek:{token}@github.com/GodUnicornIzek/SelfPlayAI.git main')
+
+
+# In[ ]:
+
+
+get_ipython().system('git status')
+
+
+# In[ ]:
+
+
+get_ipython().system('ls')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('cd', '/content/SelfPlayAI')
+
+# Rename it to a clean filename
+get_ipython().system('mv "AlphaZeroImplementation (2).ipynb" AlphaZeroImplementation.ipynb')
+get_ipython().system('rm "AlphaZeroImplementation (1).ipynb"')
+
+
+# In[ ]:
+
+
+get_ipython().system('git add AlphaZeroImplementation.ipynb')
+get_ipython().system('git commit -m "Add/update notebook"')
+
+
+# In[ ]:
+
+
+get_ipython().system('git remote set-url origin https://github.com/Godunicornizek/SelfPlayAI.git')
+
+
+# In[ ]:
+
+
+import getpass
+token = getpass.getpass("Enter GitHub token: ")
+
+get_ipython().system('git push https://GodUnicornIzek:{token}@github.com/GodUnicornizek/SelfPlayAI.git main')
+
+
+# In[ ]:
+
+
+# -------------------------------
+# Colab GitHub Push Workflow
+# -------------------------------
+
+from google.colab import files, drive
+import os, getpass
+
+# --- 1. Configure Git ---
+get_ipython().system('git config --global user.email "godunicornizek@gmail.com"')
+get_ipython().system('git config --global user.name "GodunicornIzek"')
+
+# --- 2. Clone repo (if not already present) ---
+repo_dir = "/content/SelfPlayAI"
+if not os.path.exists(repo_dir):
+    get_ipython().system('git clone https://github.com/Godunicornizek/SelfPlayAI.git {repo_dir}')
+
+# --- 3. Move into repo directory ---
+get_ipython().run_line_magic('cd', '{repo_dir}')
+
+# --- 4. Upload the notebook if not already in Colab ---
+uploaded = files.upload()  # Choose your tictactoe.ipynb
+# This will place the file in /content/, copy it into the repo
+for fname in uploaded.keys():
+    get_ipython().system('cp "/content/{fname}" "{repo_dir}/{fname}"')
+
+# --- 5. Optional: rename notebook to clean name ---
+get_ipython().system('mv "{repo_dir}/tictactoe.ipynb" "tictactoe.ipynb"')
+
+# --- 6. Stage and commit changes ---
+get_ipython().system('git add tictactoe.ipynb')
+get_ipython().system('git commit -m "Add/update TicTacToe notebook"')
+
+# --- 7. Set remote URL just in case ---
+get_ipython().system('git remote set-url origin https://github.com/Godunicornizek/SelfPlayAI.git')
+
+# --- 8. Push safely using getpass ---
+token = getpass.getpass("Enter GitHub token: ")
+get_ipython().system('git push https://GodunicornIzek:{token}@github.com/Godunicornizek/SelfPlayAI.git main')
+
+# --- 9. Status check ---
+get_ipython().system('git status')
+
